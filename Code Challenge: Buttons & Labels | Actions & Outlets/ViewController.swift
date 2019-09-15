3e53e52da391609cfe9513df7681d07a30ed6930
Omar E <omareldoronki@icloud.com>
2019-09-15 17:34:48 -0400

    renamed project

--- Code Challenge: Buttons & Labels | Actions & Outlets/ViewController.swift
+++ Code Challenge: Buttons & Labels | Actions & Outlets/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ColorLabel: UILabel!
+    
+    @IBOutlet weak var change2green: UIButton!
+    @IBAction func screenGreen(_ sender: Any) {
+        view.backgroundColor = UIColor.green
+        ColorLabel.text = "It's not easy being green."
+
+    }
+    @IBOutlet weak var change2white: UIButton!
+    @IBAction func screenWhite(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+        ColorLabel.text = "What color am I?"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
